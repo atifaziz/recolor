8267f44a482b0b52d092be0505ba61974494fab5
Atif Aziz <code@raboof.com>
2015-04-11 16:17:43 +0200

    Unused imports

--- Program.cs
+++ Program.cs
@@ -24,9 +24,6 @@ namespace Recolor
     using System.Globalization;
     using System.IO;
     using System.Linq;
-    using System.Reflection;
-    using System.Runtime.InteropServices;
-    using System.Text;
     using System.Text.RegularExpressions;
     using Mannex;
     using Mannex.Collections.Generic;
